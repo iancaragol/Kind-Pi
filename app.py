6e6c23d054f548d6bfffc11f32b72d527a419587
Ian Caragol <iancaragol@gmail.com>
2020-01-02 20:31:58 -0700

    Added simple framework and test image

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from display import Display
+from flask import Flask, render_template
+
+dh = Display()
+
+IMAGE_FOLDER = os.path.join('static', 'images')
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
+
+@app.route('/dab')
+def show_index():
+    image_filename = dh.serve_dab()
+    return render_template("output_image.html", default_image = image_filename)
+
+@app.route('/bus')
+def show_index():
+    image_filename = dh.serve_bus()
+    return render_template("output_image.html", default_image = image_filename)
+
+def main():
+    app.run()
+
+if __name__ == "__main__":
+    main()
---
+++ display.py
@@ -0,0 +1,15 @@
+from controllers.bus import Bus
+
+class Display:
+    def __init__(self):
+        self.bus_controller = Bus()
+
+    def serve_dab(self):
+        return 'dab.jpg'
+
+    def serve_calender(self):
+        if self.bus_controller.update_image():
+            return 'bus_times.jpg'
+        else:
+            return 'error.jpg'
+
