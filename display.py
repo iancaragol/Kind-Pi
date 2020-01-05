f8fb7c779857954dd7d4f7b27bc5d8e4ca72985f
Ian Caragol <iancaragol@gmail.com>
2020-01-04 17:25:21 -0700

    Added ftp

--- display.py
+++ display.py
@@ -1,15 +1,35 @@
+from datetime import datetime as dt
+import time
+import optimage
+import requests
+
 from controllers.bus import Bus
+from PIL import Image, ImageFont, ImageDraw
 
 class Display:
     def __init__(self):
         self.bus_controller = Bus([])
 
-    def serve_dab(self):
-        return 'dab.jpg'
+    def add_time(self):
+        img = Image.open("dab-gray-crushed.png")
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("arial.ttf", 72)
 
-    def serve_calender(self):
-        if self.bus_controller.update_image():
-            return 'bus_times.jpg'
-        else:
-            return 'error.jpg'
+        now = dt.now()
+        current_time = now.strftime("%H:%M")
+        draw.text((5, 5), current_time, (0),font=font)
+        img.save("dab-gray-crushed-edit.png")
 
+        img = Image.open('dab-gray-crushed-edit.png').convert('LA')
+        img.save('out.png')
+
+def main():
+    d = Display()
+
+    while(True):
+        time.sleep(60.0)
+        d.add_time()
+
+
+if __name__ == "__main__":
+    main()
