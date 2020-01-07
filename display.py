fe5a4bc7eac9ee287025be4e7244289ed2e0cdf8
iancaragol <iancaragol@gmail.com>
2020-01-07 13:14:49 -0700

    restrucuted display.py

--- display.py
+++ display.py
@@ -1,44 +1,55 @@
-from datetime import datetime as dt
 import time
 import requests
 import os
 
+from datetime import datetime as dt
 from controllers.bus import Bus
 from PIL import Image, ImageFont, ImageDraw
 
 class Display:
-    def __init__(self):
+    def __init__(self, verbose):
         self.bus_controller = Bus([])
+        self.verbose = verbose
 
-    def add_time(self):
-        img = Image.open("dab-gray-crushed.png")
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("arial.ttf", 72)
-
+    def add_time(self, draw, font):
         now = dt.now()
         current_time = now.strftime("%H:%M")
         draw.text((5, 5), current_time, (0),font=font)
+
+        if self.verbose:
+            print("Time added: {}".format(current_time))
+
+    def add_bus(self, draw, font):
+        return 
+  
+    def update_image(self):
+        img = Image.open("base_image.png")
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("arial.ttf", 72)
+
+        # Add to image here
+        self.add_time(draw, font)
+
         img.convert('L')
-        img.save("out.png")
-        print("Saved image")
+        img.save("out_pre.png")
+        if self.verbose:
+            print("Saved image")
 
-        # os.remove("out_crush.png")
-        # print("Deleted old out_crush.png")
-
-        f = os.popen("sudo pngcrush out.png out_crush.png")
+        cwd = os.getcwd()
+        f = os.popen("pngcrush {}/out_pre.png {}/out.png".format(cwd, cwd))
         x = f.read()
-        print(x)
-        #optimage._pngcrush("out.png", "out_crush.png")
-        print("Crushed image")	
+        
+        if self.verbose:
+            print(x)
 
         
 
 def main():
-    d = Display()
+    d = Display(True)
     # d.add_time()
 
     while(True):
-        d.add_time()
+        d.update_image()
         print("Sleeping for 59 seconds...")
         time.sleep(59.0)
 
