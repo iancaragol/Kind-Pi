96bc9ce24edf11fe777a446c1b55712b954f69f1
Ian Caragol <iancaragol@gmail.com>
2021-07-10 15:43:57 -0600

    Added error handling to query weather

--- modules/openweather/weatherhandler.py
+++ modules/openweather/weatherhandler.py
@@ -23,21 +23,37 @@ class WeatherHandler:
         }
 
         request_uri = self.air_quality_uri.format(self.lat, self.lon, self.api_key)
-        response = requests.get(request_uri, headers=headers)
-        #print(request_uri)
-        #print(response)
-        data = response.json()["list"][0]
         aq = {
-            "overall" : data["main"]["aqi"],
-            "co" : data["components"]["co"],
-            "no" : data["components"]["no"],
-            "no2" : data["components"]["no2"],
-            "o3" : data["components"]["o3"],
-            "so2" : data["components"]["so2"],
-            "pm2_5" : data["components"]["pm2_5"],
-            "pm10" : data["components"]["pm10"],
-            "nh3" : data["components"]["nh3"],
+                "overall" : "Default",
+                "co" : 0,
+                "no" : 0,
+                "no2" : 0,
+                "o3" : 0,
+                "so2" : 0,
+                "pm2_5" : 0,
+                "pm10" : 0,
+                "nh3" : 0,
         }
+
+        try:
+            response = requests.get(request_uri, headers=headers)
+            #print(request_uri)
+            #print(response)
+            data = response.json()["list"][0]
+            aq["overall"] = data["main"]["aqi"]
+            aq["co"] = data["components"]["co"]
+            aq["no"] = data["components"]["no"]
+            aq["no2"] = data["components"]["no2"]
+            aq["o3"] = data["components"]["o3"]
+            aq["so2"] = data["components"]["so2"]
+            aq["pm2_5"] = data["components"]["pm2_5"]
+            aq["pm10"] = data["components"]["pm10"]
+            aq["nh3"] = data["components"]["nh3"]
+
+        except Exception as e:
+            print("An exception occurred in query_air_quality_api")
+            print("Returning default values.")
+            print(e)
 
         return aq
 
@@ -49,18 +65,30 @@ class WeatherHandler:
         }
 
         request_uri = self.weather_uri.format(self.lat, self.lon, self.api_key)
-        response = requests.get(request_uri, headers=headers)
-        #print(request_uri)
-        #print(response)
-
-        data = response.json()
         we = {
-            "temp" : round(data["main"]["temp"], 2),
-            "temp_max" : round(data["main"]["temp_max"], 2),
-            "temp_min" : round(data["main"]["temp_min"], 2),
-            "wind_speed" : round(data["wind"]["speed"], 2),
-            "description" : data["weather"][0]["main"]
+            "temp" : 0,
+            "temp_max" : 0,
+            "temp_min" : 0,
+            "wind_speed" : 0,
+            "description" : 0
         }
+
+        try:
+            response = requests.get(request_uri, headers=headers)
+            #print(request_uri)
+            #print(response)
+
+            data = response.json()
+            we["temp"] = round(data["main"]["temp"], 2)
+            we["temp_max"] = round(data["main"]["temp_max"], 2)
+            we["temp_min"] = round(data["main"]["temp_min"], 2)
+            we["wind_speed"] = round(data["wind"]["speed"], 2)
+            we["description"] = data["weather"][0]["main"]
+                    
+        except Exception as e:
+            print("An exception occurred in query_air_quality_api")
+            print("Returning default values.")
+            print(e)
 
         return we
 
